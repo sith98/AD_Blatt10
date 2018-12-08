f2ed4e217feceb8f317419e63c6f779e51bb9372
Simon Thelen <sith98@web.de>
2018-12-08 17:17:38 +0100

    SkipList: complete rewrite (delete still missing)

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,111 +1,169 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 class Node<T extends Comparable<T>> {
-    int height;
-    T value;
-    List<Node<T>> next;
-
-    Node(T value, int height) {
+    private T value;
+    private int height;
+    private Node<T>[] next;
+    
+    @SuppressWarnings("unchecked cast")
+    Node(T value) {
         this.value = value;
-        this.height = height;
-        next = new ArrayList<>(height);
-        for (int i = 0; i < height; i++) {
-            next.add(null);
-        }
+        height = randomSize();
+        next = (Node<T>[])new Node[height];
     }
-
-    public void insert(Node<T> node) {
-        while (next.size() < node.height) {
-            next.add(node);
+    
+    T getValue() {
+        return value;
+    }
+    
+    int getHeight() {
+        return height;
+    }
+    
+    Node<T> getNext(int index) {
+        return next[index];
+    }
+    
+    void setNext(int index, Node<T> node) {
+        next[index] = node;
+    }
+    
+    static private int randomSize() {
+        Random random = new Random();
+        
+        int size = 1;
+        
+        while (random.nextBoolean()) {
+            size += 1;
         }
-
-        Node<T> firstNode;
-        for (int i = next.size() - 1; i >= 0; i--) {
-            firstNode = next.get(i);
-            if (firstNode != null && firstNode.value.compareTo(value) < 0) {
-                firstNode.insert(node);
+        return size;
+    }
+    
+    void insert(Node<T> node) {
+        // Try to insert node at one of my successor nodes.
+        for (int i = height - 1; i >= 0; i--) {
+            if (next[i] != null && next[i].value.compareTo(node.value) < 0) {
+                next[i].insert(node);
+                for (int j = next[i].height; j < height && j < node.height; j++) {
+                    node.next[j] = next[j];
+                    next[j] = node;
+                }
                 return;
             }
         }
-        
-        for (int i = 0; i < node.height; i++) {
-            if (next.get(i) != node) {
-                node.next.set(i, next.get(i));
-                next.set(i, node);
-            }
+        // Insert at this
+        for (int i = 0; i < height && i < node.height; i++) {
+            node.next[i] = next[i];
+            next[i] = node;
         }
+    }
+    
+    @Override
+    public String toString() {
+        return value + ": " +
+            Arrays.stream(next).map(node -> node == null ? null : node.value.toString())
+                .collect(Collectors.joining(", "));
     }
 }
 
 class SkipList<T extends Comparable<T>> {
-    ArrayList<Node<T>> heads = new ArrayList<>();
-
+    private ArrayList<Node<T>> start = new ArrayList<>();
+    
     void insert(T value) {
-        int height = randomHeight();
-        Node<T> node = new Node<>(value, height);
-
-        int previousHeight = heads.size();
-
-        while (heads.size() < node.height) {
-            heads.add(node);
+        Node<T> node = new Node<>(value);
+        int currentHeight = start.size();
+        
+        while (start.size() < node.getHeight()) {
+            start.add(node);
         }
-
-        Node<T> firstNode;
-        for (int i = previousHeight - 1; i >= 0; i--) {
-            firstNode = heads.get(i);
-            if (firstNode != null && firstNode.value.compareTo(value) < 0) {
-                firstNode.insert(node);
+        
+        for (int i = currentHeight - 1; i >= 0; i--) {
+            Node<T> currentNode = start.get(i);
+            if (currentNode != null && currentNode.getValue().compareTo(node.getValue()) < 0) {
+                currentNode.insert(node);
+                for (int j = currentNode.getHeight(); j < currentHeight && j < node.getHeight(); j++) {
+                    node.setNext(j, currentNode);
+                    start.set(j, node);
+                }
                 return;
             }
         }
-        for (int i = 0; i < node.height; i++) {
-            if (heads.get(i) != node) {
-                node.next.set(i, heads.get(i));
-                heads.set(i, node);
+        
+        for (int i = 0; i < currentHeight && i < node.getHeight(); i++) {
+            node.setNext(i, start.get(i));
+            start.set(i, node);
+        }
+    }
+    
+    boolean contains(T value) {
+        Node<T> node = null;
+        
+        for (int i = start.size() - 1; i >= 0; i--) {
+            int comparison = start.get(i).getValue().compareTo(value);
+            
+            if (comparison == 0) {
+                return true;
+            }
+            if (comparison < 0) {
+                node = start.get(i);
+                break;
             }
         }
+        
+        if (node == null) {
+            return false;
+        }
+        
+        while (true) {
+            Node<T> nextNode = null;
+            for (int i = node.getHeight() - 1; i >= 0; i--) {
+                int comparison = node.getNext(i).getValue().compareTo(value);
+                
+                if (comparison == 0) {
+                    return true;
+                }
+                
+                if (comparison < 0) {
+                    nextNode = node.getNext(i);
+                    break;
+                }
+            }
+            
+            if (nextNode == null) {
+                return false;
+            }
+            node = nextNode;
+        }
     }
-
+    
     @Override
     public String toString() {
+        Node<T> node = start.get(0);
+        
         StringBuilder builder = new StringBuilder();
-
-        Node<T> node = heads.get(0);
         while (node != null) {
-            builder
-                .append(node.value)
-                .append(": ")
-                .append(
-                    node.next.stream()
-                        .map(it -> it == null ? null : it.value.toString())
-                        .collect(Collectors.joining(", "))
-                )
-                .append('\n');
-            node = node.next.get(0);
+            builder.append(node.toString()).append('\n');
+            node = node.getNext(0);
         }
         return builder.toString();
-    }
-
-    private static int randomHeight() {
-        Random random = new Random();
-        int height = 1;
-        while (random.nextBoolean()) {
-            height += 1;
-        }
-        return height;
     }
 }
 
 public class Aufgabe1 {
     public static void main(String[] args) {
         int[] numbers = {2, 7, 12, 11, 23, 14, 8, 5};
-        SkipList<Integer> list = new SkipList<>();
+        
+        var list = new SkipList<Integer>();
+    
         for (int number : numbers) {
             list.insert(number);
             System.out.println(list);
         }
+    
+        System.out.println(list.contains(11));
+        System.out.println(list.contains(13));
     }
 }
